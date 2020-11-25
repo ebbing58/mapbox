bb73cb697a818e99fb63192f0f93217d7014c89a
ebbing58 <imperfectimpact@gmail.com>
2020-11-26 01:20:12 +0900

    Update gpsredirectliff.js

--- gpsredirectliff.js
+++ gpsredirectliff.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
 	    geo_location.getCurrentPosition(function (position){
 	        var coords = position.coords;
-			location.href = 'https://ebbing58.github.io/mapbox/?lng=${position.coords.longitude}&lat=${position.coords.latitude}&z=13';
+			location.href = 'https://ebbing58.github.io/mapbox/';//?lng=${position.coords.longitude}&lat=${position.coords.latitude}&z=13';
 
 	    });
 	}
