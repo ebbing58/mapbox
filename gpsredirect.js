04779888d3add792db45c358981871a7f55d7eab
ebbing58 <imperfectimpact@gmail.com>
2020-11-26 01:35:23 +0900

    Update gpsredirect.js

--- gpsredirect.js
+++ gpsredirect.js
@@ -7,7 +7,7 @@ window.onload = function() {
 	        var coords = position.coords;
 //			location.href = 'https://ebbing58.github.io/mapbox/?lng=${position.coords.longitude}&lat=${position.coords.latitude}&z=13';
 			console.log(position.coords);
-			location.href = 'https://ebbing58.github.io/mapbox/?lng=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&z=15';
+			location.href = 'https://ebbing58.github.io/mapbox/?lng=135.679754&lat=34.841888&z=15';
 	    });
 	}
 };
