ecfd9528e0c32fdea9887f7afc02d601b262162e
ebbing58 <imperfectimpact@gmail.com>
2020-11-28 18:01:14 +0900

    shelter

--- gpsredirect.js
+++ gpsredirect.js
@@ -2,12 +2,10 @@ window.onload = function() {
 
 	if(navigator.geolocation){
 	    var geo_location = navigator.geolocation;
-
+		//todo gpsが機能しない場合は？
 	    geo_location.getCurrentPosition(function (position){
 	        var coords = position.coords;
-			alert('https://ebbing58.github.io/mapbox/?lng=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&z=13');
 			location.href = 'https://ebbing58.github.io/mapbox/?lng=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&z=13';
-//			location.href = 'https://ebbing58.github.io/mapbox/?lng=135.679754&lat=34.841888&z=15';
 	    });
 	}
 };
